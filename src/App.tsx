6db11980f0a02db27627f667cd38aeae965a513f
yaroslavzefi <yaroslavzef@gmail.com>
2024-01-01 17:28:16 +0300

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const [isOpened, setIsOpened] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <button onClick={() => setIsOpened(true)}>Toggle</button>
+        <Modal
+            active={isOpened}
+            setActive={setIsOpened}
+        >текст
+        </Modal>
+      </div>
   );
 }
 
